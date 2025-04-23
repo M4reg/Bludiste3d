6b36bb3fb4e25af070448ea2ec03fe1dd930d993
M4reg <marekludvik1@seznam.cz>
2025-04-23 21:50:23 +0200

    Flashlight

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+
+    public GameObject ON;
+    public GameObject OFF;
+    private bool isOn;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        ON.SetActive(false);
+        OFF.SetActive(true);
+        isOn = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (isOn)
+            {
+                ON.SetActive(false);
+                OFF.SetActive(true);
+            }
+            else
+            {
+                ON.SetActive(true);
+                OFF.SetActive(false);
+            }
+            isOn = !isOn;
+        }
+    }
+}
